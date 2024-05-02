8b5d4a431cd00932eb56a48965cbcb718a13c769
Picca33 <167075342+Picca33@users.noreply.github.com>
2024-05-02 16:35:14 +0200

    Update pi_homework.c

--- HOMEWORKS/MPI/pi_homework.c
+++ HOMEWORKS/MPI/pi_homework.c
@@ -9,19 +9,18 @@
 int main(int argc, char **argv)
 {
     long int i, intervals = INTERVALS;
-    double x, dx, f, tot_sum, pi, node_sum = 0.0;
+    double x, dx, f, tot_sum = 0.0, pi, node_sum = 0.0;
     double start_time, end_time;
     int size, rank;
     
-    MPI_INIT(&argc, &argv);
+    MPI_Init(&argc, &argv);
     MPI_Comm_size(MPI_COMM_WORLD, &size);
     MPI_Comm_rank(MPI_COMM_WORLD, &rank);
 
     if(rank==0){
-      printf("Number of intervals: %ld\n", intervals);
+        printf("Number of intervals: %ld\n", intervals);
     }
-
-    sum = 0.0;
+  
     dx = 1.0 / (double) intervals;
 
     start_time = MPI_Wtime();
@@ -35,13 +34,13 @@ int main(int argc, char **argv)
     MPI_Reduce(&node_sum, &tot_sum, 1, MPI_DOUBLE, MPISUM, 0, MPI_COMM_WORLD);
 
     if(rank==0){
-      pi = dx*tot_sum;
-
-      end_time = MPI_Wtime();
+        pi = dx*tot_sum;
   
-      printf("Computed PI %.24f\n", pi);
-      printf("The true PI %.24f\n\n", PI25DT);
-      printf("Elapsed time (s) = %.2lf\n", (end_time-start_time));
+        end_time = MPI_Wtime();
+    
+        printf("Computed PI %.24f\n", pi);
+        printf("The true PI %.24f\n\n", PI25DT);
+        printf("Elapsed time (s) = %.2lf\n", (end_time-start_time));
     }
     
     MPI_Finalize();

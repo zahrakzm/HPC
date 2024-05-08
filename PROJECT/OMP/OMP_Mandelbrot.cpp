8c971b33534e00d36940d2b6d094416b7cffe8e4
Picca33 <167075342+Picca33@users.noreply.github.com>
2024-05-08 10:59:06 +0200

    Update OMP_Mandelbrot.cpp

--- PROJECT/OMP/OMP_Mandelbrot.cpp
+++ PROJECT/OMP/OMP_Mandelbrot.cpp
@@ -36,12 +36,13 @@ int main(int argc, char **argv)
     
     int num_threads = atoi(argv[1]);
     int *const image = new int[HEIGHT * WIDTH];
+    int chuncksize = 50;
 
     // start timer
     double start_time = omp_get_wtime();
 
     omp_set_num_threads(num_threads);
-    #pragma omp parallel for shared(image, WIDTH, HEIGHT, STEP, MIN_X, MIN_Y) private(pos, row, col, c, z, i) schedule(static)
+    #pragma omp parallel for shared(image, WIDTH, HEIGHT, STEP, MIN_X, MIN_Y) private(pos, row, col, c, z, i) schedule(static, chuncksize)
     for (int pos = 0; pos < HEIGHT * WIDTH; pos++)
     {
         image[pos] = 0;

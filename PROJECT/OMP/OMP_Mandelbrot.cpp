eb2e5d516e240a184b8d0843113b75d232157d15
Picca33 <167075342+Picca33@users.noreply.github.com>
2024-05-08 12:50:56 +0200

    Update OMP_Mandelbrot.cpp

--- PROJECT/OMP/OMP_Mandelbrot.cpp
+++ PROJECT/OMP/OMP_Mandelbrot.cpp
@@ -33,7 +33,7 @@ int main(int argc, char **argv)
         printf("Usage: %s <N> <num_threads>\n", argv[0]);
         return 1;
     }
-    printf("Number of threads = %s\n", argv[0]);
+    printf("Number of threads = %s\n", argv[1]);
     int num_threads = atoi(argv[1]);
     int *const image = new int[HEIGHT * WIDTH];
 

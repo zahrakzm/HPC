8270bf1950c54f91ed94d6f558bb8f6150f17ef8
Picca33 <167075342+Picca33@users.noreply.github.com>
2024-05-08 12:13:11 +0200

    Update OMP_Mandelbrot.cpp

--- PROJECT/OMP/OMP_Mandelbrot.cpp
+++ PROJECT/OMP/OMP_Mandelbrot.cpp
@@ -29,7 +29,7 @@ using namespace std;
 int main(int argc, char **argv)
 {
     // size of input array
-    if(argc != 3){
+    if(argc != 2){
         printf("Usage: %s <N> <num_threads>\n", argv[0]);
         return 1;
     }

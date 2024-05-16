3e457cf66ce3d126e8eb4f87770337f34c61e9aa
Picca33 <167075342+Picca33@users.noreply.github.com>
2024-05-16 09:05:52 +0200

    Update MPI_Mandelbrot.cpp

--- PROJECT/MPI/MPI_Mandelbrot.cpp
+++ PROJECT/MPI/MPI_Mandelbrot.cpp
@@ -38,25 +38,21 @@ int main(int argc, char **argv)
         printf("Number of nodes: %s\n", world_size);
     }
 
-    int total_pixels = HEIGHT * WIDTH;
-    int pixels_per_node = total_pixels / world_size;
-    int start_idx = world_rank * pixels_per_node;
-    int end_idx = (world_rank + 1) * pixels_per_node;
-
-    int *image;
-    int *sub_image = new int[pixels_per_node];
+    int rows_per_node = HIEGHT / world_size;
+    int start_row = world_rank * rows_per_node;
+    int *sub_image = new int[rows_per_node * WIDTH];
     double start_time = MPI_Wtime();
 
-    if(world_rank==0){
-        image = new int[total_pixels];
-    }
+    //int num_threads = atoi(argv[1]);
+    //omp_set_num_threads(num_threads);
 
-    //const auto start = chrono::steady_clock::now();
-    
-    for (int pos = start_idx; pos < end_idx; pos++)
+    //#pragma omp parallel for shared(sub_image)  
+    for (int pos = 0; pos < rows_per_process*WIDTH; pos++)
     {
-        const int row = pos / WIDTH;
-        const int col = pos % WIDTH;
+        int global_pos = (start_row*WIDTH) + pos;
+        sub_image[pos] = 0;
+        const int row = global_pos / WIDTH;
+        const int col = global_pos % WIDTH;
         const complex<double> c(col * STEP + MIN_X, row * STEP + MIN_Y);
 
         // z = z^2 + c
@@ -68,13 +64,18 @@ int main(int argc, char **argv)
             // If it is convergent
             if (abs(z) >= 2)
             {
-                sub_image[pos-start_idx] = i;
+                sub_image[pos] = i;
                 break;
             }
         }
     }
 
-    MPI_Gather(sub_image, pixels_per_node, MPI_INT, image, pixels_per_node, MPI_INT, 0, MPI_COMM_WORLD);
+    int *image = nullptr;
+    if(world_rank==0){
+        image = new int[HEIGHT*WIDTH];
+    }
+
+    MPI_Gather(sub_image, rows_per_node*WIDTH, MPI_INT, image, rows_per_node*WIDTH, MPI_INT, 0, MPI_COMM_WORLD);
 
     if(world_rank==0){
         //const auto end = chrono::steady_clock::now();
@@ -104,7 +105,6 @@ int main(int argc, char **argv)
             for (int col = 0; col < WIDTH; col++)
             {
                 matrix_out << image[row * WIDTH + col];
-    
                 if (col < WIDTH - 1)
                     matrix_out << ',';
             }

87abc6aaa2d62ef3e14eab5c53e379a0e8dea898
Zahra kazemi <48262473+zahrakzm@users.noreply.github.com>
2024-05-17 09:33:04 +0200

    Update MPI_Mandelbrot.cpp

--- PROJECT/MPI/MPI_Mandelbrot.cpp
+++ PROJECT/MPI/MPI_Mandelbrot.cpp
@@ -43,7 +43,7 @@ int main(int argc, char **argv) {
     double start_time, end_time;
     start_time = MPI_Wtime();
     //const auto start = chrono::steady_clock::now();
-    int num_threads = atoi(argv[1])
+    int num_threads = atoi(argv[1]);
     omp_set_num_threads(num_threads);
     printf("num_threads = %s", num_threads);
     

14e9ec6b44c7da514edc19c5ccf114ac018a36fb
Picca33 <167075342+Picca33@users.noreply.github.com>
2024-05-20 06:52:59 +0200

    Update MPI_Mandelbrot.cpp

--- PROJECT/MPI/MPI_Mandelbrot.cpp
+++ PROJECT/MPI/MPI_Mandelbrot.cpp
@@ -45,7 +45,10 @@ int main(int argc, char **argv) {
     //const auto start = chrono::steady_clock::now();
     int num_threads = atoi(argv[1]);
     omp_set_num_threads(num_threads);
-    cout << num_threads << "\n";
+    
+    if (rank == 0) {
+        cout << num_threads << "\n";
+    }
     
     
 #pragma omp parallel for shared(sub_image) ///for the OMP + MPI version
